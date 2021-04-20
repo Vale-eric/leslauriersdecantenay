b90fe616c9bdbc7fb626d5921c71e4dfa9a01528
RicoKidd49 <eric.vale@hotmail.fr>
2021-04-20 18:48:24 +0200

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9ea7192fa015c602650f9adc1748f8b9'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '69db9f8a-972f-4387-b727-b94b97afa40c'
+};
